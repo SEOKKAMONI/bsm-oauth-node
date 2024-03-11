dd5a751b6eac2f06c84a1725e6d7b15fcb710393
김석진(maru) <102217654+SEOKKAMONI@users.noreply.github.com>
2024-03-11 17:33:47 +0900

    refactor: APIClient를 Client와 instance로 역할을 분리해요 (#15)
    * refactor: APIClient의 역할을 분리해요
    
    * chore: changeset 업데이트

--- src/resources/token.ts
+++ src/resources/token.ts
@@ -1,13 +1,13 @@
 import { APIError } from '../error';
-import { APIClient } from '../core';
+import { Client, instance } from '../core';
 
-export class Token extends APIClient {
+export class Token extends Client {
   async get(authCode: string) {
     if (authCode == undefined) {
       throw new APIError(404, '유효하지 않은 authCode입니다.');
     }
 
-    const { data } = await this.client.post<{ token: string }>('/token', {
+    const { data } = await instance.post<{ token: string }>('/token', {
       authCode,
       ...this.options,
     });
