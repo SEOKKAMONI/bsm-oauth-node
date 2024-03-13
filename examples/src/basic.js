3905d712eddec5610095f856d7067b15485bdd11
김석진(maru) <102217654+SEOKKAMONI@users.noreply.github.com>
2024-03-13 23:58:34 +0900

    faet: bsm-oauth-node 라이브러리의 예제 코드를 제공해요 (#43)

---
+++ examples/src/basic.js
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+import express from 'express';
+import { BsmOauth, isStudent, isTeacher } from 'bsm-oauth-node';
+
+const app = express();
+
+const bsmOauth = new BsmOauth({
+  clientId: process.env.BSM_AUTH_CLIENT_ID,
+  clientSecret: process.env.BSM_AUTH_CLIENT_SECRET,
+});
+
+app.post('/oauth/bsm', async (req, res) => {
+  const { authCode } = req.query;
+
+  const token = await bsmOauth.token.get(authCode);
+  const user = await bsmOauth.user.get(token);
+
+  if (isStudent(user)) {
+    return res.status(200).json(user);
+  }
+
+  if (isTeacher(user)) {
+    return res.status(200).json(user);
+  }
+
+  return res.status(400).json({ error: 'user role이 비어있습니다.' });
+});
+
+app.listen(8088, () => {
+  console.log('시스템 가동 준비 완료');
+});
