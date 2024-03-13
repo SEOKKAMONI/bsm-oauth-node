9e0aeeb1a52bd902d6c5dbb1b9476b84c17a60a1
김석진(maru) <102217654+SEOKKAMONI@users.noreply.github.com>
2024-03-13 23:31:18 +0900

    feat: 예외 처리를 추가해요 (#41)

--- src/resources/user.ts
+++ src/resources/user.ts
@@ -9,23 +9,28 @@ export class User extends Client {
   }
 
   async get(token: string) {
-    if (token == undefined) {
-      throw new APIError(404, '유효하지 않은 token입니다.');
-    }
+    try {
+      if (token == undefined) {
+        throw new APIError(404, '유효하지 않은 token입니다.');
+      }
 
-    const data = await request<{ user: Student | Teacher }>('/resource', {
-      token,
-      ...this.options,
-    });
+      const data = await request<{ user: Student | Teacher }>('/resource', {
+        token,
+        ...this.options,
+      });
 
-    if (isStudent(data.user)) {
+      if (isStudent(data.user)) {
+        return data.user;
+      }
+
+      if (isTeacher(data.user)) {
+        return data.user;
+      }
+
       return data.user;
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
-
-    if (isTeacher(data.user)) {
-      return data.user;
-    }
-
-    return data.user;
   }
 }
