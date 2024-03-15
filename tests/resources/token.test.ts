f91df6ded14066a8cd2ca47ab6430c1d7b06dfdf
SEOKKAMONI <sj01048260279@gmail.com>
2024-03-15 23:34:39 +0900

    test: test 설명을 수정해요

--- tests/resources/token.test.ts
+++ tests/resources/token.test.ts
@@ -7,14 +7,14 @@ const bsmOauth = new BsmOauth({
 });
 
 describe('토큰 조회', () => {
-  test('유효한 authCode를 제공하면 토큰을 반환합니다.', async () => {
+  test('유효한 authCode를 제공하면 토큰을 반환해요.', async () => {
     const token = await bsmOauth.token.get('authCode');
 
     expect(token).not.toBeNull();
     expect(token).toBeDefined();
   });
 
-  test('authCode를 제공하지 않으면 에러가 발생합니다.', async () => {
+  test('authCode를 제공하지 않으면 에러가 발생해요.', async () => {
     try {
       await bsmOauth.token.get('');
     } catch (error) {
