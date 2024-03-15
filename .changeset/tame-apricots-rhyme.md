28441810d8caaafe99d269fb45f09e8fd1c99504
김석진(maru) <102217654+SEOKKAMONI@users.noreply.github.com>
2024-03-16 01:27:50 +0900

    test: user resource 테스트 코드를 추가해요 (#51)
    * test: user resource 테스트 코드를 추가해요
    
    * chore: changeset 업데이트

---
+++ .changeset/tame-apricots-rhyme.md
@@ -0,0 +1,5 @@
+---
+"bsm-oauth-node": patch
+---
+
+test: user resource 테스트 코드를 추가해요
--- tests/resources/user.test.ts
+++ tests/resources/user.test.ts
@@ -7,15 +7,15 @@ const bsmOauth = new BsmOauth({
   clientSecret: process.env.BSM_AUTH_CLIENT_SECRET,
 });
 
-describe('토큰 조회', () => {
-  test('authCode를 제공하지 않으면 에러가 발생해요.', async () => {
+describe('유저 조회', () => {
+  test('token을 제공하지 않으면 에러가 발생해요.', async () => {
     try {
-      await bsmOauth.token.get('');
+      await bsmOauth.user.get('');
     } catch (error) {
       const apiError = error as APIError;
 
       expect(apiError).toBeInstanceOf(APIError);
-      expect(apiError.message).toBe('[404]: 유효하지 않은 authCode입니다.');
+      expect(apiError.message).toBe('[404]: 유효하지 않은 token입니다.');
     }
   });
 });
